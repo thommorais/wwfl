9c420021373b7dc7a25c0f9cbe6ea056afa3b1a0
Thom Morais <thommorais@gmail.com>
2018-12-10 20:59:04 -0200

    build

--- src/background-animation/index.js
+++ src/background-animation/index.js
@@ -1,5 +1,5 @@
 export default function backgroundAnimation() {
-  import(/* webpackChunkName:"basicscroll" */ 'basicScroll').then((module) => {
+  import(/* webpackChunkName:"basicscroll" */ 'basicscroll').then((module) => {
     [].slice.call(document.querySelectorAll('.shape')).forEach((elem) => {
       const {
         modifier, y, from, to
