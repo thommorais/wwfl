6533464103705751fda65b3761127b20db291760
Thom Morais <thommorais@gmail.com>
2018-12-10 17:18:17 -0200

    adds social shares links

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import './assets/css/index.sass'
 import carousel from './game-carousel'
 import areYouPreparedModal from './game-carousel/areYouPreparedModal'
 import mapColors from './game-carousel/mapColors'
-import backgroundAnimation from './background-animation'
+// import backgroundAnimation from './background-animation'
 // import titlAnimation from './background-animation/tilt'
 
 if (PRODUCTION) {
   require('offline-plugin/runtime').install()
 }
 
-backgroundAnimation()
+// backgroundAnimation()
 // titlAnimation()
 
 import(/* webpackChunkName:"swiper" */ './lib/swiper.esm').then((module) => {
